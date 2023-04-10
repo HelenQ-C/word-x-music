c01d3156b61195977752c128e0d86719f9fa4551
HelenQ-C <cheh58008@gmail.com>
2023-04-10 10:50:09 -0400

    updare

--- material/assets/main.js
+++ material/assets/main.js
@@ -11,12 +11,75 @@ scale1.addEventListener("click", function(){
   }
 });
 
-const scale = document.querySelector('.scale1');
+const effect = document.querySelector('.effect');
 
-scale1.addEventListener('click', function() {
-  if (scale1.classList.contains('clicked')) {
-    scale1.classList.remove('clicked');
+effect.addEventListener('click', function() {
+  if (effect.classList.contains('clicked')) {
+    effect.classList.remove('clicked');
   } else {
-    scale1.classList.add('clicked');
+    effect.classList.add('clicked');
+  }
+});
+
+const scale2 = document.getElementById("Button2"); 
+const imgs2 = document.getElementById("ToToggle2");
+
+imgs2.style.display = "none";
+
+scale2.addEventListener("click", function(){
+  if (imgs2.style.display === "none") {
+    imgs2.style.display = "block";
+  } else {
+    imgs2.style.display = "none";
+  }
+});
+
+const effect2 = document.querySelector('.effect2');
+
+effect2.addEventListener('click', function() {
+  if (effect2.classList.contains('clicked')) {
+    effect2.classList.remove('clicked');
+  } else {
+    effect2.classList.add('clicked');
+  }
+});
+
+const effect3 = document.querySelector('.effect3');
+
+effect3.addEventListener('click', function() {
+  if (effect3.classList.contains('clicked')) {
+    effect3.classList.remove('clicked');
+  } else {
+    effect3.classList.add('clicked');
+  }
+});
+
+const effect4 = document.querySelector('.effect4');
+
+effect4.addEventListener('click', function() {
+  if (effect4.classList.contains('clicked')) {
+    effect4.classList.remove('clicked');
+  } else {
+    effect4.classList.add('clicked');
+  }
+});
+
+const effect5 = document.querySelector('.effect5');
+
+effect5.addEventListener('click', function() {
+  if (effect5.classList.contains('clicked')) {
+    effect5.classList.remove('clicked');
+  } else {
+    effect5.classList.add('clicked');
+  }
+});
+
+const effect6 = document.querySelector('.effect6');
+
+effect6.addEventListener('click', function() {
+  if (effect6.classList.contains('clicked')) {
+    effect6.classList.remove('clicked');
+  } else {
+    effect6.classList.add('clicked');
   }
 });
